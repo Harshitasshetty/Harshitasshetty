045bca76ae0b0fe33841384f5af66e0d1fed780a
Harshita shetty <harshitashetty1802@gmail.com>
2026-09-22 21:31:42 +0530

    Add files via upload

---
+++ c2.py
@@ -0,0 +1,38 @@
+print("Hello World")
+a = 10
+b = 20
+
+print(a+b)
+
+a, b, c = 1, 2, 3
+a = b = c = 10
+
+_name = "Harshita"
+_Name = "ashmita"
+
+print(_Name)
+print(_name)
+
+name = "Harshita" #string
+age = 18 #int
+is_student = False #bool
+weight =47.5 #flaot
+
+print(type(is_student))
+
+is_student = "yes"
+print(type(is_student))
+
+age_float = float(age)
+print(age_float)
+
+s="100"
+print(int(s)+age)
+
+
+
+
+
+
+
+
