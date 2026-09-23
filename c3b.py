a4a6e6fd8ac513f7e014b62ab75db30ac82a37ea
Harshita shetty <harshitashetty1802@gmail.com>
2026-09-23 17:36:56 +0530

    Add files via upload

---
+++ c3b.py
@@ -0,0 +1,40 @@
+first_name= "Harshita"
+last_name= "Shetty"
+full_name= first_name + " " + last_name
+print(full_name)
+
+#repetition
+message = "This is a Warning! " 
+print(message*10)
+
+print(message.upper())
+print(message.lower())
+print(message.strip()*2)
+print(message.replace("Warning! " , "Error!"))
+
+name='harshita said"hello"'
+print(name)
+
+Name=''' harshita said "hello"
+and harshi said "hi" '''
+print(Name)
+
+print(len(message)) #length
+
+name="harshita"
+print(name[5]) #index = position-1
+print(name[:8])
+print(name[1:])
+print(name[-2])
+print(name[::2])
+
+s=" harshita \nis a good girl "
+print(s)
+
+s=" harshita \tis a good girl "
+print(s)
+
+S=" harshita \\is a good girl"
+print(s)
+
+
