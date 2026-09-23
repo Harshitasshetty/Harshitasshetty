35a7f3953aafd5b6dba2f26c302ebb12fa5f5fc5
Harshita shetty <harshitashetty1802@gmail.com>
2026-09-23 17:29:51 +0530

    Add files via upload

---
+++ c3.py
@@ -0,0 +1,38 @@
+
+print("friends")
+
+boy_name=input("Boy Name: ")
+boy_age=int(input("Boy Age: "))
+girl_name=input("Girl Name: ")
+girl_age=int(input("Girl Age: "))
+
+age_diff = abs(boy_age - girl_age)
+#using abs becausesomtimes boy might be younger
+
+print(boy_name + " friends " + girl_name +". Age Difference is " + str(age_diff))
+print(f"{boy_name} friends {girl_name}. Age Difference is {age_diff}")
+
+'''
+this
+is a multline 
+comment
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
