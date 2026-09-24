95e327631a6bb438d209a691c617868aefa6e2a9
Harshita shetty <harshitashetty1802@gmail.com>
2026-09-24 19:43:44 +0530

    Add files via upload

---
+++ c4.py
@@ -0,0 +1,37 @@
+x=None
+print(type(x))
+
+#operators
+a =10
+a += 100
+print(a)
+
+a=10
+b=20
+print(a==b)
+print(a<b)
+print(a!=b)
+
+print(True and True)
+print(False or True)
+print(not(True))
+
+print(2>1 or 2<1)
+print(2<1 or 2>1)
+print(2<1 and 2>1)
+print(2>1 and 2>1)
+print(2<1 and 2<1)
+
+s="harshita"
+print("h" in s)
+print("s" in s)
+print("x" not in s)
+print("r" not in s)
+
+s2="harshi"
+print (("h"in s) and ("h" in s2))
+print(("t" in s) or ("t" in s2))
+print (("t"in s) and ("t" in s2))
+print(not ("t" in s2))
+
+
