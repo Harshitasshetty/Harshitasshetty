bfe3d37f5464c47a95bbf869ed7ba00c2deabf32
Harshita shetty <harshitashetty1802@gmail.com>
2026-09-25 19:40:32 +0530

    Add files via upload

---
+++ c5.py
@@ -0,0 +1,37 @@
+#lists
+item1 = "bru"
+item2 ="Milk"
+print(item1 , item2)
+
+items = ["bru" , "sugar" , "milk"  , "bru2" ]
+print(items)
+print(items[0])
+print(items[-1 ])
+items.pop()
+print(items)
+items.append("mom's magic")
+print(items)
+items.remove("sugar")
+print(items)
+items.insert(1,"tea power")
+print(items)
+items[0] = "coffee power"
+print(items)
+print(len(items))
+
+l=[1 ,"bru" ,True ,[1234]]
+print(l)
+
+a=[700,500,300,400]
+print(a[1:2:2])
+print(sorted(a))
+print(sum(a))
+
+m=[ [1, 2], [3,4]]
+print(m[0][1])
+print(m) # nesting
+print(type(m))
+
+
+
+
